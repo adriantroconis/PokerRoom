0d9e52b9d549c9209a12105aa2099dbef43fc2b4
adriantroconis <adrian_troconis2000@hotmail.com>
2017-06-29 15:52:10 -0400

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=csci3300_poker', 'csci3300_poker', '8duK8rat2ehuyedR');
+    $db = new PDO('mysql:host=pokercriollo.com.ve;dbname=pokercriollo_heroku', 'pokercriollo_heroku', '1813mami.');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     $error = $e->getMessage();
